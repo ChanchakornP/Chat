42a03a14d5ab3c925c9a68050f7a659a47908319
ChanchakornP <gamechanchakorn@gmail.com>
2024-07-28 21:19:55 +1000

    update llm response after finish streaming

--- app/db/routes.py
+++ app/db/routes.py
@@ -42,30 +42,26 @@ def create_user():
 @mysql.route("/users/chats", methods=["PUT"])
 def update_user_chat():
     data = request.json
-    user_id = session.get("user_id")
     chat_id = data.get("chat_id")
-    message_raw = data.get("message")
-
-    user = User.query.filter_by(id=user_id).first()
-    # Error handling
-    if not user:
-        return jsonify({"error": "User not found"}), 404
-    if not chat_id:
-        return jsonify({"error": "Chat ID not found"}), 404
-    if not message_raw:
-        return jsonify({"error": "Messages not found"}), 404
+    chat_message = data.get("chat_message")
+    sender = data.get("sender")
+    # if not chat_id:
+    #     return jsonify({"error": "Chat ID not found"}), 404
+    # if not chat_message:
+    #     return jsonify({"error": "Messages not found"}), 404
+    # if sender not in ["user", "assistant", "system"]:
+    #     return jsonify({"error": "Sender is not correct"}), 404
 
     conversation = Conversation.query.get(chat_id)
     if not conversation:
         return jsonify({"error": "Conversation not found"}), 404
 
-    for msg in message_raw:
-        message = Message(
-            conversation_id=chat_id,  # Associate message with the conversation
-            sender=msg.get("role"),
-            chat_message=msg.get("content"),
-        )
-        db.session.add(message)
+    message = Message(
+        conversation_id=chat_id,  # Associate message with the conversation
+        sender=sender,
+        chat_message=chat_message,
+    )
+    db.session.add(message)
     db.session.commit()
     return jsonify({"message": "Chat History updated"}), 201
 
@@ -73,26 +69,35 @@ def update_user_chat():
 @mysql.route("/users/chats", methods=["POST"])
 def create_user_chat():
     data = request.json
-    user_id = session.get("user_id")
+    user_id = data.get("user_id")
     message_raw = data.get("message")
-
     user = User.query.filter_by(id=user_id).first()
     # Error handling
     if not user:
         return jsonify({"error": "User not found"}), 404
-    if not message_raw:
-        return jsonify({"error": "Messages not found"}), 404
+    # if not message_raw:
+    #     return jsonify({"error": "Messages not found"}), 404
     chat_id = str(uuid.uuid4())
-    message = []
-    for msg in message_raw:
-        message.append(
-            Message(
-                conversation_id=chat_id,  # Associate message with the conversation
-                sender=msg.get("role"),
-                chat_message=msg.get("content"),
-            )
-        )
 
+    if isinstance(message_raw, dict):
+        message = [
+            Message(
+                conversation_id=message_raw.get("conversation_id"),
+                sender=message_raw.get("sender"),
+                chat_message=message_raw.get("chat_message"),
+            )
+        ]
+    elif isinstance(message_raw, list):
+        message = [
+            Message(
+                conversation_id=message_raw_.get("conversation_id"),
+                sender=message_raw_.get("sender"),
+                chat_message=message_raw_.get("chat_message"),
+            )
+            for message_raw_ in message_raw
+        ]
+    else:
+        raise "Error"
     new_conversation = Conversation(id=chat_id, user_id=user_id, message=message)
     db.session.add(new_conversation)
     db.session.commit()
@@ -146,16 +151,22 @@ def get_user_chat_content(user_id, chat_id):
     return jsonify({"user_id": user_id, "chat_messages": chat_messages})
 
 
-@mysql.route("/users/<string:user_id>/chats/<string:chat_id>", methods=["DELETE"])
-def delete_user_chat(user_id, chat_id):
-    # Validate user_id and chat_id parameters
-    chat_history = Message.query.filter_by(id=chat_id, user_id=user_id).first()
+@mysql.route("/users/chats/<string:chat_id>", methods=["DELETE"])
+def delete_user_chat(chat_id):
+    user_id = session.get("user_id")
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"error": "User not found"}), 404
 
+    chat_history = Conversation.query.filter_by(id=chat_id, user_id=user_id).first()
     if not chat_history:
         return (
             jsonify({"error": "Chat message not found or does not belong to the user"}),
             404,
         )
+    messages = Message.query.filter_by(conversation_id=chat_id).all()
+    for message in messages:
+        db.session.delete(message)
 
     db.session.delete(chat_history)
     db.session.commit()
--- app/llama3/routes.py
+++ app/llama3/routes.py
@@ -1,24 +1,107 @@
 import json
 import os
+import uuid
+from datetime import datetime
 
 import requests
-from flask import Blueprint, jsonify, request
+from celery import shared_task
+from flask import Blueprint, Response, current_app, redirect, request, session, url_for
 
 chat = Blueprint("chat", __name__)
 url = os.getenv("ENDPOINT_LLAMA3", "http://localhost:11434/api/chat")
+headers = {"Content-Type": "application/json"}
+
+
+@chat.route("/init", methods=["POST"])
+def init_chat():
+    body = request.json
+    user_prompt = body.get("user_prompt")
+    user_id = session.get("user_id")
+    chat_id = str(uuid.uuid4())
+    create_user_chat_url = current_app.config["BASE_URL"] + url_for(
+        "mysql.create_user_chat"
+    )
+    request_body = {
+        "user_id": user_id,
+        "message": {
+            "conversation_id": chat_id,
+            "sender": "user",
+            "chat_message": user_prompt,
+        },
+    }
+    init_chat_history_db_response = requests.post(
+        create_user_chat_url, json=request_body
+    )
+    if init_chat_history_db_response.status_code == 201:
+        chat_id = init_chat_history_db_response.json().get("conversation_id")
+        return redirect(url_for("homepage.index", chat_id=chat_id))
 
 
 @chat.route("/chat", methods=["POST"])
 def chat_interface():
-    if request.method == "POST":
-        if request.content_type != "application/json":
-            return jsonify({"error": "Unsupported Media Type"}), 415
+    body = request.json
+    user_prompt = body.get("user_prompt")
+    chat_id = body.get("chat_id")
+    user_id = session.get("user_id")
+    chat_history = get_chat_history(chat_id, user_id, user_prompt)
+    buffer = []
+    update_chat_history_task.delay(chat_id, user_id, user_prompt, sender="user")
 
-        data = request.json
-        headers = {"Content-Type": "application/json"}
+    def generate():
+        with requests.post(
+            url,
+            headers=headers,
+            json={"model": "llama3", "messages": chat_history},
+            stream=True,
+        ) as response:
+            for chunk in response.iter_lines(decode_unicode=True):
+                if chunk:
+                    content = json.loads(chunk)["message"]["content"]
+                    buffer.append(content)
+                    yield content
+        update_chat_history_task.delay(chat_id, user_id, buffer, sender="assistant")
 
-        response = requests.post(url, headers=headers, json=data, stream=True)
+    return Response(generate(), content_type="text/plain")
 
-        return response.raw
 
-    return jsonify({"error": "Method not allowed"}), 405
+def get_chat_history(chat_id, user_id, user_prompt):
+    get_chat_history_url = current_app.config["BASE_URL"] + url_for(
+        "mysql.get_user_chat_content", chat_id=chat_id, user_id=user_id
+    )
+    get_chat_history_response = requests.get(get_chat_history_url)
+    if get_chat_history_response == 200:
+        messages = get_chat_history_response.json()["chat_messages"]["messages"]
+        sorted_messages = sorted(
+            messages,
+            key=lambda x: datetime.strptime(
+                x["created_at"], "%a, %d %b %Y %H:%M:%S GMT"
+            ),
+        )
+        chat_history = [
+            {
+                "role": (
+                    "user"
+                    if message["sender"] == "user"
+                    else "assistant" if message["sender"] == "assistant" else "system"
+                ),
+                "content": message["chat_message"],
+            }
+            for message in sorted_messages
+        ]
+    else:
+        chat_history = []
+
+    chat_history.append({"role": "user", "content": user_prompt})
+    return chat_history
+
+
+@shared_task(name="update_chat_history_task")
+def update_chat_history_task(chat_id, user_id, message, sender):
+    update_chat_history_url = current_app.url_for("mysql.update_user_chat")
+    body = {
+        "chat_id": chat_id,
+        "chat_message": "".join(message),
+        "sender": sender,
+    }
+    response = requests.put(url=update_chat_history_url, json=body)
+    response.raise_for_status()
